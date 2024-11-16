4cc9b6cbf18a8e8b5ac8b03b667fb193a0c21675
rc4b <rcaborelli@gmail.com>
2024-11-16 18:26:01 -0300

    Inicia projeto curso VIM Alura

---
+++ README.md
@@ -0,0 +1,1 @@
+#Treinando o compartilhamento de repsitórios no GIT HUB ao compartilhar projeto do curso de VIM da Alura
---
+++ assets/js/testimonies.js
@@ -0,0 +1,30 @@
+(function(){
+	
+	var active_id = 1;
+	var testimonies_count = 3;
+	var change_testimonie = function(new_id) {
+		document.querySelector("#figure-"+active_id+"-testimones").classList.remove("active");
+		document.querySelector("#button-"+active_id+"-testimonies").classList.remove("active");
+		document.querySelector("#figure-"+new_id+"-testimones").classList.add("active");
+		document.querySelector("#button-"+new_id+"-testimonies").classList.add("active");
+		active_id = new_id;
+	};
+
+	document.querySelector(".left").addEventListener("click", function() {
+		change_testimonie(active_id - 1 + testimonies_count * (active_id == 1));
+	});
+
+	document.querySelector(".right").addEventListener("click", function() {
+		change_testimonie(active_id + 1 - testimonies_count * (active_id == testimonies_count));
+	});
+	
+	for (var i = 1; i<=testimonies_count; i++ ){
+		(function(id){
+			document.querySelector("#button-"+id+"-testimonies").addEventListener("click", function() {
+				change_testimonie(id);
+			});
+		})(i);
+	};			
+})();
+
+
